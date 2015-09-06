13e0fd27b6bb828925e0f97772da0cf30bd1b73e
Jerome Etienne <jerome.etienne@gmail.com>
2015-09-06 17:28:32 +0100

    initial commit

---
+++ shadertoys/basic-red.glsl
@@ -0,0 +1,12 @@
+uniform vec3	iResolution;
+uniform vec4	iMouse;
+float	iGlobalTime;
+
+uniform vec2 resolution;
+uniform float time;
+
+void main()	{
+	
+	gl_FragColor=vec4(1.0, 0.0,0.0,1.0);
+	
+}
